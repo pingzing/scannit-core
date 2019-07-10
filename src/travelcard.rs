b50bfe5bc95af5806c6315cc8672f1c51f970548
pingzing <mcalistern@gmail.com>
2019-07-10 21:01:57 +0300

    Some more additions to the PeriodPass. Not currently compiling.

--- src/travelcard.rs
+++ src/travelcard.rs
@@ -1,8 +1,8 @@
 use crate::conversion;
 use crate::en1545date::from_en1545_date;
-use std::convert::TryInto;
 use byteorder::{BigEndian, ByteOrder};
 use chrono::prelude::*;
+use std::convert::TryInto;
 
 pub struct TravelCard {
     // Application Info
@@ -20,6 +20,33 @@ pub struct TravelCard {
     pub action_list_counter: u32,
 }
 
+pub enum ProductCode {
+    FaresFor2010(u16), // Code type = 0
+    FaresFor2014(u16), // Code type = 1
+}
+
+pub enum BoardingLocation {
+    BusNumber(u16),
+    TrainNumber(u16),    
+    PlatformNumber(u16),
+}
+
+pub enum ValidityAreaType {
+    Zone = 0,
+    Vehicle = 1,
+}
+
+pub enum ValidityArea {
+    ZoneA = 0,
+    ZoneB = 1,
+    ZoneC = 2,
+    ZoneD = 3,
+    ZoneE = 4,
+    ZoneF = 5,
+    ZoneG = 6,
+    ZoneH = 7,
+}
+
 pub fn create_travel_card(
     app_info: &[u8],
     control_info: &[u8],
@@ -32,6 +59,7 @@ pub fn create_travel_card(
         read_application_info(app_info);
     let (issue_date, app_status, unblock_number, transaction_counter, action_counter) =
         read_control_info(control_info);
+    let period_pass = read_period_pass(period_pass);
 
     TravelCard {
         application_version: app_version,
@@ -45,6 +73,33 @@ pub fn create_travel_card(
         application_transaction_counter: transaction_counter,
         action_list_counter: action_counter,
     }
+}
+
+struct PeriodPass {
+    product_code_1: ProductCode,    
+    validity_area_1_type: ValidityAreaType,
+    validity_area_1: ValidityArea,
+    period_start_date_1: Date<Utc>,
+    period_end_date_1: Date<Utc>,
+
+    product_code_2: ProductCode,    
+    validity_area_2_type: ValidityAreaType,
+    validity_area_2: ValidityArea,
+    period_start_date_2: Date<Utc>,
+    period_end_date_2: Date<Utc>,
+
+    // Most recent card load:
+    loaded_period_product: ProductCode,    
+    period_loading_datetime: DateTime<Utc>,
+    loaded_period_length: u16,
+    loaded_period_price: u32, // in cents
+    loading_organization: u16,
+    loading_device_number: u16,
+
+    // Last use/boarding:
+    last_board_datetime: DateTime<Utc>,
+    last_board_vehicle: u16, // vehicle ID
+    last_board_location: BoardingLocation
 }
 
 // Notes about travel card data: All data is presented as a pile of bytes,
@@ -66,7 +121,15 @@ fn read_control_info(control_info: &[u8]) -> (DateTime<Utc>, bool, u8, u32, u32)
         from_en1545_date((date) >> 2), // Shift out the least-significant two bits, dates are only 14-bits long.
         control_info[1] & 0x2 != 0,    // 1-bit app status (no idea what status *means*, but...)
         control_info[2],               // 8-bit 'unblocking number' (ditto, no idea)
-        BigEndian::read_uint(&control_info[3..6], 24).try_into().unwrap(), // Application transaction counter, 24-bits long
+        BigEndian::read_uint(&control_info[3..6], 24)
+            .try_into()
+            .unwrap(), // Application transaction counter, 24-bits long
         BigEndian::read_u32(&control_info[6..10]), // Action List Counter, 32-bits long
     )
+}
+
+fn read_period_pass(period_pass: &[u8]) -> PeriodPass {
+    PeriodPass {        
+
+    }
 }
