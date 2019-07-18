f5b50cce97382f149bfc70e5d367141a4605a59f
Neil McAlister <neil.mcalister@futurice.com>
2019-07-18 16:38:04 +0300

    Proof-of-concept FFI library

---
+++ scannit-core-ffi/src/lib.rs
@@ -0,0 +1,54 @@
+use libc::c_char;
+use std::ffi::CString;
+
+#[no_mangle]
+pub extern "C" fn get_string() -> *const c_char {
+    let example_string = String::from("I am a string from Rust.");
+    let c_string = CString::new(example_string).unwrap();
+
+    c_string.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn free_string(s: *mut c_char) {
+    unsafe {
+        if s.is_null() {
+            return;
+        }
+        CString::from_raw(s)
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn get_vector() -> Buffer {
+    let strings_vector = vec!["Rust string one".to_string(), "Rust string two".to_string()];
+    let mut buffer: Vec<*mut i8> = strings_vector
+        .into_iter()
+        .map(|x| CString::new(x).unwrap().into_raw())
+        .collect();
+    //.into_boxed_slice();
+    let data = buffer.as_mut_ptr();
+    let len = buffer.len();
+    std::mem::forget(buffer); // Leak the memory so we don't auto-drop it
+    Buffer { data, len }
+}
+
+#[no_mangle]
+pub extern "C" fn free_vector(buf: Buffer) {
+    let vector = unsafe { std::slice::from_raw_parts_mut(buf.data, buf.len) };
+    for string in vector.iter() {
+        free_string(*string);
+    }
+    let vector = vector.as_mut_ptr();
+    unsafe {
+        // Take ownership of the data pointed to by the box,
+        Box::from_raw(vector);
+        // ...and destroy it at the end of scope.
+    }
+}
+
+#[repr(C)]
+pub struct Buffer {
+    data: *mut *mut c_char,
+    len: usize,
+}
