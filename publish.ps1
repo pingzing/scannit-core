1958d6cdc3de1060f599a1b22cf06d6fda197eef
Neil McAlister <neil.mcalister@futurice.com>
2019-08-30 16:10:02 +0300

    Make publish script go get cargo

--- publish.ps1
+++ publish.ps1
@@ -1,20 +1,32 @@
+# Install Rust
+$cargoPath = "";
+if ($env:AGENT_OS -eq "Windows_NT") {
+    curl -sSf -o rustup-init.exe https://win.rustup.rs;
+    rustup-init.exe -y --default-toolchain stable;
+    $cargoPath = "~/.cargo/bin";    
+}
+else {
+    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable    
+    $cargoPath = "~/.cargo/bin";
+}
+
 $regex = "version = `"(\d.\d).\d";
 
 # Rewrite version for main project
-$rewrittenMainToml = (Get-Content .\Cargo.toml) -replace $regex, "`$1.$env:BUILD_BUILDNUMBER";
-Set-Content \.Cargo.toml $rewrittenMainToml;
+$rewrittenMainToml = (Get-Content ./Cargo.toml) -replace $regex, "`$1.$env:BUILD_BUILDNUMBER";
+Set-Content /.Cargo.toml $rewrittenMainToml;
 Write-Host "Rewrote version for main Cargo.toml.";
 
 # Rewrite version for FFI project
-$rewrittenFfiToml = (Get-Content .\scannit-core-ffi\Cargo.toml) -replace $regex, "`$1.$env:BUILD_BUILDNUMBER";
-Set-Content .\scannit-core-ffi\Cargo.toml $rewrittenFfiToml;
+$rewrittenFfiToml = (Get-Content ./scannit-core-ffi/Cargo.toml) -replace $regex, "`$1.$env:BUILD_BUILDNUMBER";
+Set-Content ./scannit-core-ffi/Cargo.toml $rewrittenFfiToml;
 Write-Host "Rewrote version for FFI Cargo.toml.";
 
 # CARGO_API_KEY is a secret env var, and should be replaced by Azure DevOps.
 # Publish main project
-cargo login $(CARGO_API_KEY);
-cargo publish;
+& $cargoPath/cargo cargo login $(CARGO_API_KEY);
+& $cargoPath/cargo publish;
 
 # Publish FFI project
-Set-Location .\scannit-core-ffi;
-cargo publish;
+Set-Location ./scannit-core-ffi;
+& $cargoPath/cargo publish;
