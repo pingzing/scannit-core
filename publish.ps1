e6c53d3dae4a00cc6809008b7651574fd3f5ba24
pingzing <mcalistern@gmail.com>
2019-10-27 17:44:59 +0200

    Fix path issue in publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -14,20 +14,18 @@ $regex = "version = `"\d.\d.\d";
 
 # Rewrite version for main project
 $rewrittenMainToml = (Get-Content ./Cargo.toml) -replace $regex, "version = `"$env:BUILD_BUILDNUMBER";
-Set-Content /.Cargo.toml $rewrittenMainToml;
+Set-Content ./Cargo.toml $rewrittenMainToml;
 Write-Host "Rewrote version for main Cargo.toml.";
-Write-Host "New content is: $($rewrittenMainToml)";
 
 # Rewrite version for FFI project
 $rewrittenFfiToml = (Get-Content ./scannit-core-ffi/Cargo.toml) -replace $regex, "version = `"$env:BUILD_BUILDNUMBER";
 Set-Content ./scannit-core-ffi/Cargo.toml $rewrittenFfiToml;
 Write-Host "Rewrote version for FFI Cargo.toml.";
-Write-Host "New content is: $($rewrittenFfiToml)";
 
 # CARGO_API_KEY is a secret env var, and should be replaced by Azure DevOps.
 # Publish main project
 & $cargoPath/cargo login $env:CargoApiKey;
-& $cargoPath/cargo publish;
+& $cargoPath/cargo publish --allow-dirty;
 
 # Publish FFI project
 Set-Location ./scannit-core-ffi;
