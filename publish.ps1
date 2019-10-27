ccb136fb123024b89931a9c82b739a239d6f7f50
pingzing <mcalistern@gmail.com>
2019-10-27 17:14:16 +0200

    Fix some versioning stuff

--- publish.ps1
+++ publish.ps1
@@ -13,13 +13,13 @@ else {
 $regex = "version = `"(\d.\d).\d";
 
 # Rewrite version for main project
-$rewrittenMainToml = (Get-Content ./Cargo.toml) -replace $regex, "`$1.$env:BUILD_BUILDNUMBER";
+$rewrittenMainToml = (Get-Content ./Cargo.toml) -replace $regex, "version = `"1.0.$env:BUILD_BUILDNUMBER";
 Set-Content /.Cargo.toml $rewrittenMainToml;
 Write-Host "Rewrote version for main Cargo.toml.";
 Write-Host "New content is: $($rewrittenMainToml)";
 
 # Rewrite version for FFI project
-$rewrittenFfiToml = (Get-Content ./scannit-core-ffi/Cargo.toml) -replace $regex, "`$1.$env:BUILD_BUILDNUMBER";
+$rewrittenFfiToml = (Get-Content ./scannit-core-ffi/Cargo.toml) -replace $regex, "version = `"1.0.$env:BUILD_BUILDNUMBER";
 Set-Content ./scannit-core-ffi/Cargo.toml $rewrittenFfiToml;
 Write-Host "Rewrote version for FFI Cargo.toml.";
 Write-Host "New content is: $($rewrittenFfiToml)";
